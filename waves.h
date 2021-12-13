9ec28866fcd7e44b2a0dd95cbb13592fd87c862c
Markus Videfors <markus.videfors@outlook.com>
2021-12-13 15:47:47 +0100

    Add waves from python script

---
+++ waves.h
@@ -0,0 +1,13 @@
+#define ticks_per_sample 1000
+extern char G4[];
+extern int G4length;
+extern char A4[];
+extern int A4length;
+extern char B4[];
+extern int B4length;
+extern char C5[];
+extern int C5length;
+extern char D5[];
+extern int D5length;
+extern char E5[];
+extern int E5length;
