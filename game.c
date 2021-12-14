9472e62a8f90dd7a42325c113a5048fb9937c6ee
Markus Videfors <markus.videfors@outlook.com>
2021-12-14 12:03:12 +0100

    oijsadojias

--- game.c
+++ game.c
@@ -1,7 +1,8 @@
+#include "audio.h"
 #include "graphics.h"
-#include "object.h"
-#include "image_arrays.h"
 #include "highscore.h"
+#include "image_arrays.h"
+#include "object.h"
 #include <pic32mx.h> /* Declarations of system-specific addresses etc */
 
 int ingame = 0;
@@ -9,27 +10,24 @@ screenstate *current_level_screen;
 
 /// Resets game variables and starts the game by setting ingame = 1
 /// The game loop will then run in the sound ISR
-void play_level(int level)
-{
-    current_score = 0;
-    player_jumpflag = 1;
-    player.vel.x = 0;
-    player.vel.y = 0;
-    player.pos.x = 4;
-    player.pos.y = 0;
-    if (level == 0)
-    {
-        current_level_screen = &stage1_screen;
-    }
-    else if (level == 1)
-    {
-        current_level_screen = &stage2_screen;
-    }
-    current_level_screen->current_scroll_amount = 0;
-    ingame = 1; // enables game logic in song_isr
-    while (ingame)
-        ; // loop until game over;
-    song_stop();
+void play_level(int level) {
+  current_score = 0;
+  player_jumpflag = 1;
+  player.vel.x = 0;
+  player.vel.y = 0;
+  player.pos.x = 4;
+  player.pos.y = 0;
+  if (level == 0) {
+    current_level_screen = &stage1_screen;
+  } else if (level == 1) {
+    current_level_screen = &stage2_screen;
+  }
+  current_level_screen->current_scroll_amount = 0;
+  ingame = 1; // enables game logic in song_isr
+  song_init(level);
+  while (ingame)
+    ; // loop until game over;
+  song_stop();
 }
 
 void timer_init() {
@@ -40,92 +38,78 @@ void timer_init() {
   IFSCLR(0) = 0x00001000; // Make sure the time-out flag is cleared
   T3CONSET = 0x8000;      // Turn the timer on again
   IPCSET(3) = 0x1f;       // Set priority to max
-  IECSET(0) = 0x1000;     // Enable interupts for Timer 2
+  IECSET(0) = 0x1000;     // Enable interupts for Timer 3
 }
 
-
 /// Ends the game
-void game_over()
-{
-    ingame = 0;
-    if (current_score > current_highscore)
-    {
-        current_highscore = current_score;
-        show_enter_highscore_screen();
-    } else{
-        refresh_screen(&game_over_screen);
-        put_short_string(0, "",0);
-        put_short_string(1, "   GAME OVER",12);
-        put_short_string(2, "   YOU LOST",11);
-        put_short_string(0, "",0);
-        add_textbuffer_to_screen(&game_over_screen);
-        draw_entire_display(&game_over_screen);
-        quicksleep(100000000);
-    }
-    return;
+void game_over() {
+  ingame = 0;
+  if (current_score > current_highscore) {
+    current_highscore = current_score;
+    show_enter_highscore_screen();
+  } else {
+    refresh_screen(&game_over_screen);
+    put_short_string(0, "", 0);
+    put_short_string(1, "   GAME OVER", 12);
+    put_short_string(2, "   YOU LOST", 11);
+    put_short_string(0, "", 0);
+    add_textbuffer_to_screen(&game_over_screen);
+    draw_entire_display(&game_over_screen);
+    quicksleep(5000000);
+  }
+  return;
 }
 
 /// Moves the level and the player and updates game graphics.
-void update_game()
-{
-    // If the game hasn't progressed to its end
-    if (current_level_screen->current_scroll_amount <= current_level_screen->entire_image_width - 128)
-    {
-        if (!btn_dwn_flag)
-        {
-            if (PORTF & 0b10 && player_jumpflag == 0)
-            {
-                player.vel.y = -5;
-                player_jumpflag = 1;
-                btn_dwn_flag = 1;
-            }
+void update_game() {
+  // If the game hasn't progressed to its end
+  if (current_level_screen->current_scroll_amount <=
+      current_level_screen->entire_image_width - 128) {
+    if (!btn_dwn_flag) {
+      if (PORTF & 0b10 && player_jumpflag == 0) {
+        player.vel.y = -5;
+        player_jumpflag = 1;
+        btn_dwn_flag = 1;
+      }
 
-            if ((PORTD & 0b1000000) && player.pos.x > 0)
-            { // move left
-                player.vel.x = -1;
-                btn_dwn_flag = 1;
-            }
-            else if ((PORTD & 0b100000) && player.pos.x < 127)
-            { // move right
-                player.vel.x = 1;
-                btn_dwn_flag = 1;
-            }
-        }
-        else if (!(PORTF & 0b10 || PORTD & 0b11100000))
-        {
-            btn_dwn_flag = 0;
-            player.vel.x = 0;
-        }
+      if ((PORTD & 0b1000000) && player.pos.x > 0) { // move left
+        player.vel.x = -1;
+        btn_dwn_flag = 1;
+      } else if ((PORTD & 0b100000) && player.pos.x < 127) { // move right
+        player.vel.x = 1;
+        btn_dwn_flag = 1;
+      }
+    } else if (!(PORTF & 0b10 || PORTD & 0b11100000)) {
+      btn_dwn_flag = 0;
+      player.vel.x = 0;
+    }
 
-        //         För att kolla om programmet kraschar:
-        //  if (PORTE == 1){
-        //      PORTE = 0;
-        //  } else{
-        //      PORTE = 1;
-        //  }
-        refresh_screen(current_level_screen);
-        move_background(current_level_screen, 1);
-        if (move_object(current_level_screen, &player))
-        { // if game is over
-            btn_dwn_flag = 1;
-            return;
-        }
-        add_object_to_screen(&player, current_level_screen);
-        draw_entire_display(current_level_screen);
-        // Highscore is percentage of level completed
-        current_score = (current_level_screen->current_scroll_amount * 100) / (current_level_screen->entire_image_width - 128);
+    //         För att kolla om programmet kraschar:
+    //  if (PORTE == 1){
+    //      PORTE = 0;
+    //  } else{
+    //      PORTE = 1;
+    //  }
+    refresh_screen(current_level_screen);
+    move_background(current_level_screen, 1);
+    if (move_object(current_level_screen, &player)) { // if game is over
+      btn_dwn_flag = 1;
+      return;
     }
-    else
-    {
-        refresh_screen(&game_over_screen);
-        put_string(0, "YOU WON!!!!!!!!!");
-        put_string(1, "YOU WON!!!!!!!!!");
-        put_string(2, "YOU WON!!!!!!!!!");
-        put_string(3, "YOU WON!!!!!!!!!");
-        add_textbuffer_to_screen(&game_over_screen);
-        draw_entire_display(&game_over_screen);
-        quicksleep(100000000);
-        game_over();
-    }
+    add_object_to_screen(&player, current_level_screen);
+    draw_entire_display(current_level_screen);
+    // Highscore is percentage of level completed
+    current_score = (current_level_screen->current_scroll_amount * 100) /
+                    (current_level_screen->entire_image_width - 128);
+  } else {
+    refresh_screen(&game_over_screen);
+    put_string(0, "YOU WON!!!!!!!!!");
+    put_string(1, "YOU WON!!!!!!!!!");
+    put_string(2, "YOU WON!!!!!!!!!");
+    put_string(3, "YOU WON!!!!!!!!!");
+    add_textbuffer_to_screen(&game_over_screen);
+    draw_entire_display(&game_over_screen);
+    quicksleep(5000000);
+    game_over();
+  }
 }
-
--- menu.c
+++ menu.c
@@ -1,105 +1,74 @@
-#include "image_arrays.h"
+#include "game.h"
 #include "graphics.h"
 #include "highscore.h"
-#include "game.h"
-#include <pic32mx.h> /* Declarations of system-specific addresses etc */
-#include "screenstate.h"
+#include "image_arrays.h"
 #include "object.h"
-#include "highscore.h"
+#include "screenstate.h"
+#include <pic32mx.h> /* Declarations of system-specific addresses etc */
 
 /// Menu where you select which level to play
-void level_selection()
-{
-    uint8_t selected = 0;
-    selectarrow.pos.y = 23;
-    while (1)
-    {
-        if (!btn_dwn_flag)
-        {
-            if (PORTF & 0b10)
-            { // if btn1
-                btn_dwn_flag = 1;
-                song_init(selected);
-                play_level(selected);
-            }
+void level_selection() {
+  uint8_t selected = 0;
+  selectarrow.pos.y = 23;
+  while (1) {
+    if (!btn_dwn_flag) {
+      if (PORTF & 0b10) { // if btn1
+        btn_dwn_flag = 1;
+        play_level(selected);
+      }
 
-            refresh_screen(&level_selection_screen);
-            if (PORTD & 0b100000)
-            { // if btn2
-                selected = 1;
-            }
-            else if (PORTD & 0b1000000)
-            { // if btn3
-                selected = 0;
-            }
-            else if (PORTD & 0b10000000)
-            { //if btn 4
-                return;
-            }
-            if (selected)
-            {
-                selectarrow.pos.x = 92;
-            }
-            else
-            {
-                selectarrow.pos.x = 25;
-            }
-            add_object_to_screen(&selectarrow, &level_selection_screen);
-            draw_entire_display(&level_selection_screen);
-        }
-        else if (!(PORTF & 0b10 || PORTD & 0b11100000))
-        {
-            btn_dwn_flag = 0;
-        }
+      refresh_screen(&level_selection_screen);
+      if (PORTD & 0b100000) { // if btn2
+        selected = 1;
+      } else if (PORTD & 0b1000000) { // if btn3
+        selected = 0;
+      } else if (PORTD & 0b10000000) { // if btn 4
+        return;
+      }
+      if (selected) {
+        selectarrow.pos.x = 92;
+      } else {
+        selectarrow.pos.x = 25;
+      }
+      add_object_to_screen(&selectarrow, &level_selection_screen);
+      draw_entire_display(&level_selection_screen);
+    } else if (!(PORTF & 0b10 || PORTD & 0b11100000)) {
+      btn_dwn_flag = 0;
     }
+  }
 }
 
 /// Displays the main menu interface
-void main_menu()
-{
-    uint8_t selected = 0;
-    selectarrow.pos.y = 28;
-    while (1)
-    {
+void main_menu() {
+  uint8_t selected = 0;
+  selectarrow.pos.y = 28;
+  while (1) {
 
-        if (!btn_dwn_flag)
-        {
-            if (PORTF & 0b10)
-            { // if btn1
-                btn_dwn_flag = 1;
-                if (selected)
-                {
-                    show_highscore_screen();
-                }
-                else
-                {
-                    level_selection();
-                }
-            }
+    if (!btn_dwn_flag) {
+      if (PORTF & 0b10) { // if btn1
+        btn_dwn_flag = 1;
+        if (selected) {
+          show_highscore_screen();
+        } else {
+          level_selection();
+        }
+      }
 
-            refresh_screen(&main_menu_screen);
-            if (PORTD & 0b100000)
-            { // if btn2
-                selected = 1;
-            }
-            else if (PORTD & 0b1000000)
-            { // if btn3
-                selected = 0;
-            }
-            if (selected)
-            {
-                selectarrow.pos.x = 98;
-            }
-            else
-            {
-                selectarrow.pos.x = 28;
-            }
-            add_object_to_screen(&selectarrow, &main_menu_screen);
-            draw_entire_display(&main_menu_screen);
-        }
-        else if (!(PORTF & 0b10 || PORTD & 0b11100000))
-        {
-            btn_dwn_flag = 0;
-        }
+      refresh_screen(&main_menu_screen);
+      if (PORTD & 0b100000) { // if btn2
+        selected = 1;
+      } else if (PORTD & 0b1000000) { // if btn3
+        selected = 0;
+      }
+      if (selected) {
+        selectarrow.pos.x = 98;
+      } else {
+        selectarrow.pos.x = 28;
+      }
+      add_object_to_screen(&selectarrow, &main_menu_screen);
+      draw_entire_display(&main_menu_screen);
+    } else if (!(PORTF & 0b10 || PORTD & 0b11100000)) {
+      btn_dwn_flag = 0;
     }
+  }
 }
